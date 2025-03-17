94ef731c530e8b5797aab11bdf92db2f1f70463a
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-03-17 16:14:47 +0530

    API Requests for Accounts

--- obj/Debug/net8.0/AdminService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdminService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a809b593630e54e5715ea1c4e40dcd7cf5d2d66b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a86ccc9d8e43b7aef4cdbbf7b1eb8e171a758ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
