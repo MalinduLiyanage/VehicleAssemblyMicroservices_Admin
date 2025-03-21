c5cb0aeda7b7d22b0283ea4f612c0eb8b2d12b53
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-03-21 12:12:52 +0530

    Minor Code Revisions

--- obj/Debug/net8.0/AdminService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdminService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab036b261891c4e2aa4a16f0d0ae67fcc30d5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f330a1e84587677c9ca77382e0ada5cdfa20f272")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
